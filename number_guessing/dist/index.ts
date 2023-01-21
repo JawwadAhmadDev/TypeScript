31993b771fbb091e7798377633312648c8bbc19b
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-01-21 18:26:27 +0500

    updated

--- number_guessing/dist/index.ts
+++ number_guessing/dist/index.ts
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-import inquirer from "inquirer";
+// import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 import { createSpinner } from 'nanospinner';
--- number_guessing/src/sleep.js
+++
@@ -1,2 +0,0 @@
-const sleep = (ms = 2000) => new Promise((res, rej) => setTimeout(res, ms));
-export default sleep;
